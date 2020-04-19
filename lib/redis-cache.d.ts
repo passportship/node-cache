33fa234e8a8ea65c8b2a3047dd62d395255ba4a7
Vladimir Rudenko <ruden.v.a@gmail.com>
2020-04-19 20:37:11 +0600

    Renamed isSharedDatabase property

--- lib/redis-cache.d.ts
+++ lib/redis-cache.d.ts
@@ -1,11 +1,11 @@
 import { AsyncBaseCache } from './async-base-cache';
 /**
- * > Note: If you need to share database, you should set [[isShareDatabase]] to `true` and make sure that
+ * > Note: If you need to share database, you should set [[isSharedDatabase]] to `true` and make sure that
  * > [[keyPrefix]] has unique value which will allow to distinguish between cache keys and other data in database.
  */
 export declare class RedisCache extends AsyncBaseCache {
     private readonly _client;
-    isShareDatabase: boolean;
+    isSharedDatabase: boolean;
     readonly client: any;
     constructor(options?: any, clientOptions?: any);
     runCommand(command: any, ...args: any[]): Promise<any>;
